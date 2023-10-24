749bc9c0dda1a8084da1d5918eae1797a32937d2
1rehmansha2 <1rehmansha2@hdsb.ca>
2023-10-24 14:06:53 -0400

    no moving to staging dir

--- src/ElderBloody/Title.java
+++ src/ElderBloody/Title.java
@@ -3,7 +3,7 @@ package ElderBloody;
 	public class Title {
 		
 		// This string is made by an online generator, just a good font for ASCII art that I found
-		final String titleText = 
+		final static String titleText = 
 				"▓█████  ██▓    ▓█████▄ ▓█████  ██▀███     \r\n"
 				+ "▓█   ▀ ▓██▒    ▒██▀ ██▌▓█   ▀ ▓██ ▒ ██▒   \r\n"
 				+ "▒███   ▒██░    ░██   █▌▒███   ▓██ ░▄█ ▒   \r\n"
@@ -26,11 +26,10 @@ package ElderBloody;
 				+ "      ░                            ░      ";
 		
 		// String ANSI_PURPLE contains the color code for a purple background in text form 
-		final String ANSI_PURPLE = "\u001B[45m";
+		final static String ANSI_PURPLE = "\u001B[45m";
 		
 		/* String elderBlood concatenates the String ANSI_PURPLE with the ASCII art, which gives us our
 		 * ASCII art title with a purple background, and the default foreground (text) color, which is white.
 		 */
-		final String elderBlood = ANSI_PURPLE + titleText;
+		final static String elderBlood = ANSI_PURPLE + titleText;
 	}
-}
