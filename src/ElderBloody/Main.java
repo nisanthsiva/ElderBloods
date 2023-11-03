db0a01d7629752d0483d53ad9be24377c0338892
Nisanth <nisanthsk8@gmail.com>
2023-11-03 13:12:30 -0400

    added room class

--- src/ElderBloody/Main.java
+++ src/ElderBloody/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Title.printAbsPath();
+		Room booleansFirstRoom = new Room(1, true, false, true, true); // North, East, South, West
+		booleansFirstRoom.drawRoom();
 	}
 }
