0e064cb9041527cd3c2592c54bb805ca10ce8e31
Nisanth <114787282+nisanthsiva@users.noreply.github.com>
2023-11-09 13:31:18 -0500

    Update Main.java

--- src/ElderBloody/Main.java
+++ src/ElderBloody/Main.java
@@ -11,10 +11,10 @@ public class Main {
 	private static int gamestate = 0;
 	
 	
-	private static Room[][] map = {
-			{}, new Room(1, 1, 2, 11, 1), new Room(2, 2, 3, 12, 1), new Room(3, 3, 4, 13, 2), new Room(4, 4, 5, 4, 3), new Room(5, 5, 6, 15, 4), new Room(6, 6, 6, 16, 5), new Room(7, 7, 8, 17, 7), new Room(8, 8, 9, 8, 7), new Room(9, 9, 10, 19, 8), new Room(10, 10, 10, 20, 9)
-			{}, new Room(11, 1, 12, 21, 11), new Room(12, 2, 13, 22, 11), new Room(13, 3, 14, 13, 12), new Room(14, 14, 15, 24, 13), new Room(15, 5, 15, 25, 15), new Room(16, 6, 17, 16, 16), new Room(17, 7, 17, 17, 16), new Room(18, 18, 19, 28, 18), new Room(19, 9, 19, 19, 18), new Room(20, 10, 20, 30, 20)
-			{}, new Room(21, 11, 21, 31, 21), new Room(22, 12, 22, 32, 22), new Room(23, 23, 24, 33, 23), new Room(24, 14, 25, 34, 23), new Room(25, 15, 26, 35, 24), new Room(26, 26, 27, 26, 25), new Room(27, 27, 28, 37, 26), new Room(28, 18, 29, 38, 27), new Room(29, 29, 30, 29, 28), new Room(30, 20, 30, 40, 29)
+	private static Room[] map = {
+			new Room(1, 1, 2, 11, 1), new Room(2, 2, 3, 12, 1), new Room(3, 3, 4, 13, 2), new Room(4, 4, 5, 4, 3), new Room(5, 5, 6, 15, 4), new Room(6, 6, 6, 16, 5), new Room(7, 7, 8, 17, 7), new Room(8, 8, 9, 8, 7), new Room(9, 9, 10, 19, 8), new Room(10, 10, 10, 20, 9),
+			new Room(11, 1, 12, 21, 11), new Room(12, 2, 13, 22, 11), new Room(13, 3, 14, 13, 12), new Room(14, 14, 15, 24, 13), new Room(15, 5, 15, 25, 15), new Room(16, 6, 17, 16, 16), new Room(17, 7, 17, 17, 16), new Room(18, 18, 19, 28, 18), new Room(19, 9, 19, 19, 18), new Room(20, 10, 20, 30, 20),
+			new Room(21, 11, 21, 31, 21), new Room(22, 12, 22, 32, 22), new Room(23, 23, 24, 33, 23), new Room(24, 14, 25, 34, 23), new Room(25, 15, 26, 35, 24), new Room(26, 26, 27, 26, 25), new Room(27, 27, 28, 37, 26), new Room(28, 18, 29, 38, 27), new Room(29, 29, 30, 29, 28), new Room(30, 20, 30, 40, 29),
 	};
 
 	public static void main(String[] args) {
