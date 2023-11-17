10d331e87a8c237bce6295933a9a2af467fd3d5c
muhibali <125319312+muhibali@users.noreply.github.com>
2023-11-16 19:23:39 -0500

    Added more lines

--- src/ElderBloody/Story.java
+++ src/ElderBloody/Story.java
@@ -41,9 +41,9 @@ public class Story {
 		} else if(Main.getCurrentRoom() == 14) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 15) {
-			System.out.println("");
+			System.out.println("No materials here.");
 		} else if(Main.getCurrentRoom() == 16) {
-			System.out.println("");
+			System.out.println("No materials here.");
 		} else if(Main.getCurrentRoom() == 17) {
 			System.out.println("Danger! A mob has spawned and wants to fight you! Use your new powers to defeat it!"); // Route 1
 		} else if(Main.getCurrentRoom() == 18) {
@@ -133,7 +133,7 @@ public class Story {
 		} else if(Main.getCurrentRoom() == 60) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 61) {
-			System.out.println("");
+			System.out.println("Woah! You encountered an enemy. Fight them!"); // Route 2
 		} else if(Main.getCurrentRoom() == 62) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 63) {
@@ -143,31 +143,31 @@ public class Story {
 		} else if(Main.getCurrentRoom() == 65) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 66) {
-			System.out.println("");
+			System.out.println("The boss room is above! If you're ready, proceed."); // Route 2
 		} else if(Main.getCurrentRoom() == 67) {
-			System.out.println("");
+			System.out.println("Woah! An enemy is here! Fight them!"); // Route 2
 		} else if(Main.getCurrentRoom() == 68) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 69) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 70) {
-			System.out.println("");
+			System.out.println("Woah! You encountered an enemy. Fight them!");
 		} else if(Main.getCurrentRoom() == 71) {
-			System.out.println("");
+			System.out.println("There's a health station here! Use it if needed."); // Route 2
 		} else if(Main.getCurrentRoom() == 72) {
-			System.out.println("");
+			System.out.println("Woah! You encountered an enemy. Fight them!"); // Route 2
 		} else if(Main.getCurrentRoom() == 73) {
-			System.out.println("");
+			System.out.println("You found a stone!"); // Route 2
 		} else if(Main.getCurrentRoom() == 74) {
-			System.out.println("");
+			System.out.println("No materials here."); // Route 2
 		} else if(Main.getCurrentRoom() == 75) {
-			System.out.println("");
+			System.out.println("There's a training station here. Train your attacks if needed."); // Route 2
 		} else if(Main.getCurrentRoom() == 76) {
-			System.out.println("");
+			System.out.println("Woah! There's an enemy here! Fight them!"); // Route 2
 		} else if(Main.getCurrentRoom() == 77) {
-			System.out.println("");
+			System.out.println("No materials here.");
 		} else if(Main.getCurrentRoom() == 78) {
-			System.out.println("");
+			System.out.println("There's a training station here. Train attacks if you'd like."); // Route 2
 		} else if(Main.getCurrentRoom() == 79) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 80) {
@@ -177,17 +177,17 @@ public class Story {
 		} else if(Main.getCurrentRoom() == 82) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 83) {
-			System.out.println("");
+			System.out.println("There's a healing station here. Use if needed."); // Route 2
 		} else if(Main.getCurrentRoom() == 84) {
-			System.out.println("");
+			System.out.println("There's a training lab here! Train your attacks for later!"); // Route 2
 		} else if(Main.getCurrentRoom() == 85) {
-			System.out.println("");
+			System.out.println("No materials here."); // Route 2
 		} else if(Main.getCurrentRoom() == 86) {
-			System.out.println("");
+			System.out.println("There's some scrap parts here. With these, you can make more weapons! You made an axe and shovel!"); // Route 2
 		} else if(Main.getCurrentRoom() == 87) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 88) {
-			System.out.println("");
+			System.out.println("A boulder fell on you! The force on you allowed for you to gain super strength! Your attacks will be updated due to this."); // Route 2
 		} else if(Main.getCurrentRoom() == 89) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 90) {
@@ -197,22 +197,24 @@ public class Story {
 		} else if(Main.getCurrentRoom() == 92) {
 			System.out.println("");
 		} else if(Main.getCurrentRoom() == 93) {
-			System.out.println("");
+			System.out.println("You found a stick of metal! If you have a rock already, you can craft a sword!"); // Route 2
 		} else if(Main.getCurrentRoom() == 94) {
-			System.out.println("");
+			System.out.println("Woah! There's an enemy here! Fight them!"); // Route 2
 		} else if(Main.getCurrentRoom() == 95) {
-			System.out.println("");
+			System.out.println("There's a healing station here, use if needed."); // Route 2
 		} else if(Main.getCurrentRoom() == 96) {
-			System.out.println("");
+			System.out.println("There's a healing station here, use if needed."); // Route 2
 		} else if(Main.getCurrentRoom() == 97) {
-			System.out.println("");
+			System.out.println("Woah! There's an enemy here! Fight them!"); // Route 2
 		} else if(Main.getCurrentRoom() == 98) {
-			System.out.println("");
+			System.out.println("Warning, move up! If you go right, there may be consequences."); // Route 2
 		} else if(Main.getCurrentRoom() == 99) {
-			System.out.println("");
+			System.out.println("OH NO! A PORTAL TO THE BOSS ROOM!"); // Filler Room
 		} 
 	}
 	/* Route 1: 0, 1, 2, 3, 4, 5, 15, 
 	 * 16, 6, 7, 8, 18, 17, 27, 37, 47, 46, 56 */
+	/* Route 2: 0, 10, 20, 30, 31, 41, 42, 52, 51, 61, 71, 72,
+	 * 73, 74, 84, 83, 93, 94, 95, 85, 75, 76, 86, 96, 97, 98,
+	   88, 78, 77, 67, 66, 56 */
 }
-
