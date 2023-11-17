caafb6f257160511ba517b16e127f3be2da77521
tahairfan06 <145050516+tahairfan06@users.noreply.github.com>
2023-11-17 14:04:29 -0500

    Update Enemy.java

--- src/ElderBloody/Enemy.java
+++ src/ElderBloody/Enemy.java
@@ -1,42 +1,51 @@
 package ElderBloody; 
 
 public class Enemy {
-    private String name;
-    private int health;
-    private int damage;
-
-    public Enemy(String name, int health, int damage) {
-        this.name = name;
-        this.health = health;
+    int damage;
+    int speed;
+    int health;
+    String name;
+    
+    public Enemy(int damage, int speed, int health, String name) {
         this.damage = damage;
+        this.speed = speed;
+        this.health = health;
+        this.name = name;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
+    
     public int getDamage() {
         return damage;
     }
-
-    public void takeDamage(int damage) {
-        health -= damage;
-        if (health < 0) {
-            health = 0;
+    
+    public int getSpeed() {
+        return speed;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    public void receiveDamage(int damageTaken) {
+       this.health -= damageTaken;
+        if (this.health < 0) {
+           this. health = 0; 
         }
+        
+        System.out.println(name + " received " + damageTaken + " damage!");
+        System.out.println(name + "'s remaining health: " + health);
+    }
+    
+    public void chillingMist(Player player) {
+
+        int damageDealt = 18;
+   	 System.out.println(this.name  + " performs Chilling Mist on " + player.getName() + " dealing " + damageDealt + " damage!");
+        player.receiveDamage(damageDealt);
+       
     }
 
-    public boolean isAlive() {
-        return health > 0;
-    }
+	public int getHealth() {
+		
+		return health;
+	}
 
-    public void attack(Player player) {
-        int playerDamage = damage;
-        player.takeDamage(playerDamage);
-        System.out.println(name + " attacks " + player.getName() + " for " + playerDamage + " damage!");
-    }
+  
 }
