589fc4efc02f9bae0cce323ba5a7bcf9af06f2b7
tahairfan06 <145050516+tahairfan06@users.noreply.github.com>
2023-11-20 14:23:24 -0500

    Itro sceen

---
+++ src/ElderBloody/IntroScreen.java
@@ -0,0 +1,16 @@
+package ElderBloody;
+
+import java.util.Scanner;
+
+public class IntroScreen {
+	static String pName;
+	
+public static void askName(Scanner scanner) {
+	System.out.println("What is your name hero?!");
+	 pName = scanner.nextLine();
+	
+}
+	
+	
+}
+	
