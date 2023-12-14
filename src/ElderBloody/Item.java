d2d042aa3c8a8534cb04adbc666723903d6f02e1
tahairfan06 <145050516+tahairfan06@users.noreply.github.com>
2023-12-14 10:02:23 -0500

    Update Item.java

--- src/ElderBloody/Item.java
+++ src/ElderBloody/Item.java
@@ -7,18 +7,18 @@ public class Item {
 	int durability;
 	String type; // weapon or consumable (healing)
 	
-	public Item(String itemName, int multiplier, int durability, String type) {	
+	public Item(String itemName, int multiplier, String type) {	
 		this.itemName = itemName;
 		this.multiplier = multiplier;
-		this.durability = durability;
 		this.type = type;
 	}
 	
+	public void addDamage() {
+		Main.player.setDamage(Main.player.getDamage() + this.multiplier);
+	}
+	
 	public void useItem() {
-		if(this.type.equals("weapon")) { // deal damage
-			Main.player.setDamage(Main.player.getHealth() + this.multiplier);
-		}
-		else if(this.type.equals("consumable")) { // heal player
+		if(this.type.equals("consumable")) { // heal player
 			Main.player.setHealth(Main.player.getHealth() + this.multiplier);
 		}
 	}
