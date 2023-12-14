737cc79131503b4a0af51b69e7e54e847eda3557
muhibali <125319312+muhibali@users.noreply.github.com>
2023-12-14 15:30:45 -0500

    comments

--- src/ElderBloody/Item.java
+++ src/ElderBloody/Item.java
@@ -1,26 +1,36 @@
+/*****************************
+ *   Nisanth, Taha, Muhib    *
+ *        Item.java          *
+ *                           *
+ *         ICS 4U1           *
+ *                           *
+ *       Elder Bloods        *
+ *    December 14, 2023      *
+ ****************************/
+
 package ElderBloody;
 
 public class Item {
-	
+
 	String itemName;
 	int multiplier; // Damage or healing (weapon vs consumable)
 	int durability;
 	String type; // weapon or consumable (healing)
-	
-	public Item(String itemName, int multiplier, String type) {	
+
+	public Item(String itemName, int multiplier, String type) { // Constructor.
 		this.itemName = itemName;
 		this.multiplier = multiplier;
 		this.type = type;
 	}
-	
-	public void addDamage() {
+
+	public void addDamage() { // Method used to add damage to the weapon.
 		Main.player.setDamage(Main.player.getDamage() + this.multiplier);
 	}
-	
+
 	public void useItem() {
-		if(this.type.equals("consumable")) { // heal player
+		if (this.type.equals("consumable")) { // heal player
 			Main.player.setHealth(Main.player.getHealth() + this.multiplier);
 		}
 	}
-	
+
 }
