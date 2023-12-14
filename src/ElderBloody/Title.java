839f0ced34424f31507f900c235f385fb00de29a
muhibali <125319312+muhibali@users.noreply.github.com>
2023-12-14 15:33:45 -0500

    comments

--- src/ElderBloody/Title.java
+++ src/ElderBloody/Title.java
@@ -1,22 +1,14 @@
+/*****************************
+ *   Nisanth, Taha, Muhib    *
+ *        Title.java         *
+ *                           *
+ *         ICS 4U1           *
+ *                           *
+ *       Elder Bloods        *
+ *    December 14, 2023      *
+ ****************************/
+
 package ElderBloody;
-
-// Broken code remember to ask mr P for help
-/*
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-public class Title{
-	public static void printTitle() throws IOException{ 
-		final BufferedReader bob = new BufferedReader(new FileReader("art.txt"));
-		final String purpleBg = "\u001B[45m";
-		String titleText;
-		while((titleText = bob.readLine()) != null) {
-			System.out.println(purpleBg + titleText);
-		}
-	}
-}
-*/
 
 public class Title{
 	private static final String yellowFg = "\u001B[33m"; // ANSI color code for a yellow foreground
