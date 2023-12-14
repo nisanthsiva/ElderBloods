f510b0317fdc23a9c881f188e7b42310b3d31d28
muhibali <125319312+muhibali@users.noreply.github.com>
2023-12-14 15:34:12 -0500

    comments

--- src/ElderBloody/IntroScreen.java
+++ src/ElderBloody/IntroScreen.java
@@ -1,16 +1,24 @@
+/*****************************
+ *   Nisanth, Taha, Muhib    *
+ *    IntroScreen.java       *
+ *                           *
+ *         ICS 4U1           *
+ *                           *
+ *       Elder Bloods        *
+ *    December 14, 2023      *
+ ****************************/
+
 package ElderBloody;
 
-import java.util.Scanner;
+import java.util.Scanner; // Imports scanner.
 
 public class IntroScreen {
-	static String pName;
-	
-public static void askName(Scanner scanner) {
-	System.out.println("What is your name hero?!");
-	 pName = scanner.nextLine();
-	
+	static String pName; // Used to contain the user's name.
+
+	public static void askName(Scanner scanner) {
+		System.out.println("What is your name hero?!"); // Intro message.
+		pName = scanner.nextLine();
+
+	}
+
 }
-	
-	
-}
-	
