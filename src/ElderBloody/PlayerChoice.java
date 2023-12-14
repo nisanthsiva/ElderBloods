931afe7b5b636828ee44fc7ea480538db865c94b
tahairfan06 <145050516+tahairfan06@users.noreply.github.com>
2023-12-14 10:02:03 -0500

    Update PlayerChoice.java

--- src/ElderBloody/PlayerChoice.java
+++ src/ElderBloody/PlayerChoice.java
@@ -1,29 +1,57 @@
 package ElderBloody;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PlayerChoice {
-
+	  public static boolean lightningActive; // determines if you've been struck by lightning or not
 	
-	  public static void playerChoice(int num, Enemy enemy, Player player, Scanner scanner) {
-
-	        if (num == 1) {
+	  public static void playerChoice(String input, Enemy enemy, Player player, Scanner scanner)throws InterruptedException {
+		
+	        if (input.equals("1")) {
 	            System.out.println("1. Slam\t 2. Lion's Claw");
-	            System.out.println("3. Shadow Slash\t 4. Chilling Mist");
-	            int attack = scanner.nextInt();
+	            System.out.println("3. Shadow Slash\t 4. Lighting Fury");
+	            try {  //  trys it,
+	            	int attack = scanner.nextInt(); // takes the user intput as an int
 	            if (attack == 1) {
-	            	enemy.performRandomAttack(player);
-	            	player.slam(enemy);
+	            	
+	            	enemy.performRandomAttack(player); // calls a static method in enemy
+	            	Thread.sleep(3000); // waits 3 seconds
+	            	player.slam(enemy); // calls a method in player
+	            	Thread.sleep(3000); 
+	            	
 	            } else if (attack == 2) {
-	            	enemy.performRandomAttack(player);
+	            	enemy.performRandomAttack(player); 
+	            	Thread.sleep(3000);
 	                player.lionsClaw(enemy);
+	                Thread.sleep(3000);
+	                
 	            } else if (attack == 3) {
 	            	enemy.performRandomAttack(player);
+	            	Thread.sleep(3000);
 	            	player.shadowSlash(enemy);
-	            } else if (attack == 4) {
+	            	Thread.sleep(3000);
+	 
+	            } 
+	            
+	            if(lightningActive && attack == 4) { 
 	            	enemy.performRandomAttack(player);
-	                player.chillingMist(enemy);
+	            	Thread.sleep(3000);
+	                player.lightningFury(enemy);
+	                Thread.sleep(3000);
+	      
 	            }
-	        } else if (num == 2) {
+	            
+	            else if(!lightningActive && attack == 4) {
+	            		System.out.println("You Musnt Do That Now!"); // if you haven't been struck by lightning
+	            		Thread.sleep(1000);
+	            	}
+	           
+	            }
+	            catch(InputMismatchException e){ //  if the user doesn't enter an int then it catches the input mismatch
+	            	scanner.nextLine();
+	            }
+	        }
+	         else if (input.equals("2")) {
 	   			// access inventory
 	   			System.out.println("Press 'U' to use an item.");
 	   			System.out.println("Press 'X' to drop an item.");
@@ -32,17 +60,27 @@ public class PlayerChoice {
 	    		if(playerInput.equals("U")) {
 	    			System.out.println("Which item slot would you like to use?");
 	    			int itemSlot = scanner.nextInt();
+	    			try {
 	    			Inventory.useItem(itemSlot);
+	    			}
+	    			catch(ArrayIndexOutOfBoundsException e) {
+	    				System.out.println("Error");
+	    			}
 	    		}    		
 	    		else if(playerInput.equals("X")) {
 	    			System.out.println("Which item slot would you like to drop?");
 	    			int itemSlot = scanner.nextInt();
 	    			Inventory.dropItem(itemSlot);
 	    			}
-	        } else if (num == 3) {
+	        } else if (input.equals("3")) {
 	            System.out.println("you seriously thought?");
-	        } else if (num > 3 || num < 0) {
-	            // invalid input
+	        } else {
+	        	
 	        }
 	    }
-}
+	  
+	  	
+	  
+	  }
+
+
