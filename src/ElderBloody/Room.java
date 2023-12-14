075ca82cd773c15276c7fc1d41a59bad9e782640
muhibali <125319312+muhibali@users.noreply.github.com>
2023-12-14 15:32:01 -0500

    comments

--- src/ElderBloody/Room.java
+++ src/ElderBloody/Room.java
@@ -1,30 +1,41 @@
+/*****************************
+ *   Nisanth, Taha, Muhib    *
+ *        Room.java          *
+ *                           *
+ *         ICS 4U1           *
+ *                           *
+ *       Elder Bloods        *
+ *    December 14, 2023      *
+ ****************************/
+
 package ElderBloody;
 
 public class Room {
 
-	private int roomNumber; 
+	// Int variables that track rooms next to one another.
+	private int roomNumber;
 	private int roomNorth;
 	private int roomEast;
 	private int roomSouth;
 	private int roomWest;
-	private boolean alreadyExplored = false; // important for things like 
-	
-	public Room(int roomNumber, int roomNorth, int roomEast, int roomSouth, int roomWest) {
+	private boolean alreadyExplored = false; // Important when going through the story and not having things repeat.
+
+	public Room(int roomNumber, int roomNorth, int roomEast, int roomSouth, int roomWest) { // Constructor.
 		this.roomNumber = roomNumber;
 		this.roomNorth = roomNorth;
 		this.roomEast = roomEast;
 		this.roomSouth = roomSouth;
 		this.roomWest = roomWest;
 	}
-	
-	public void drawRoom() {
+
+	public void drawRoom() { // Method used to draw the rooms.
 		boolean exitNorth = this.roomNorth != roomNumber;
 		boolean exitEast = this.roomEast != roomNumber;
 		boolean exitSouth = this.roomSouth != roomNumber;
 		boolean exitWest = this.roomWest != roomNumber;
-		
-		if(exitNorth && exitEast && exitSouth && exitWest) {
-	    //if(ID == 0) {
+
+		if (exitNorth && exitEast && exitSouth && exitWest) { // 4 exits.
+			// if(ID == 0) {
 			System.out.println("      |    |      ");
 			System.out.println("      |	   |	  ");
 			System.out.println("______|    |______");
@@ -35,8 +46,8 @@ public class Room {
 			System.out.println("      |    |      ");
 		}
 		// Three Ways
-		else if(!exitNorth && exitEast && exitSouth && exitWest) {
-		//else if(ID == 1) {
+		else if (!exitNorth && exitEast && exitSouth && exitWest) { // 3 exits.
+			// else if(ID == 1) {
 			System.out.println("                  ");
 			System.out.println("                  ");
 			System.out.println("__________________");
@@ -45,9 +56,8 @@ public class Room {
 			System.out.println("      |    |      ");
 			System.out.println("      |    |      ");
 			System.out.println("      |    |      ");
-		}
-		else if(exitNorth && !exitEast && exitSouth && exitWest) {
-		//else if(ID == 2) {
+		} else if (exitNorth && !exitEast && exitSouth && exitWest) {
+			// else if(ID == 2) {
 			System.out.println("      |    |      ");
 			System.out.println("      |	   |	  ");
 			System.out.println("______|    |      ");
@@ -56,9 +66,8 @@ public class Room {
 			System.out.println("      |    |	  ");
 			System.out.println("      |	   |      ");
 			System.out.println("      |    |      ");
-		}
-		else if(exitNorth && exitEast && !exitSouth && exitWest) {
-		//else if(ID == 3) {
+		} else if (exitNorth && exitEast && !exitSouth && exitWest) {
+			// else if(ID == 3) {
 			System.out.println("      |    |      ");
 			System.out.println("      |	   |	  ");
 			System.out.println("______|    |______");
@@ -67,9 +76,8 @@ public class Room {
 			System.out.println("            	  ");
 			System.out.println("                  ");
 			System.out.println("                  ");
-		}
-		else if(exitNorth && exitEast && exitSouth && !exitWest) {
-		//else if(ID == 4) {
+		} else if (exitNorth && exitEast && exitSouth && !exitWest) {
+			// else if(ID == 4) {
 			System.out.println("      |    |      ");
 			System.out.println("      |	   |	  ");
 			System.out.println("      |    |______");
@@ -80,8 +88,8 @@ public class Room {
 			System.out.println("      |    |      ");
 		}
 		// Two Ways
-		else if(exitNorth && !exitEast && !exitSouth && exitWest) {
-		//else if(ID == 5) {
+		else if (exitNorth && !exitEast && !exitSouth && exitWest) { // 2 exits.
+			// else if(ID == 5) {
 			System.out.println("      |    |      ");
 			System.out.println("      |	   |	  ");
 			System.out.println("______|    |      ");
@@ -90,9 +98,8 @@ public class Room {
 			System.out.println(" 	        	  ");
 			System.out.println(" 	              ");
 			System.out.println("                  ");
-		}
-		else if(exitNorth && exitEast && !exitSouth && !exitWest) {
-		//else if(ID == 6) {
+		} else if (exitNorth && exitEast && !exitSouth && !exitWest) {
+			// else if(ID == 6) {
 			System.out.println("      |    |      ");
 			System.out.println("      |    |	  ");
 			System.out.println("      |    |______");
@@ -101,9 +108,8 @@ public class Room {
 			System.out.println("	        	  ");
 			System.out.println(" 	              ");
 			System.out.println("                  ");
-		}
-		else if(!exitNorth && exitEast && exitSouth && !exitWest) {
-		//else if(ID == 7) {
+		} else if (!exitNorth && exitEast && exitSouth && !exitWest) {
+			// else if(ID == 7) {
 			System.out.println("                  ");
 			System.out.println(" 	        	  ");
 			System.out.println("       ___________");
@@ -112,9 +118,8 @@ public class Room {
 			System.out.println("      |    |	  ");
 			System.out.println("      |	   |      ");
 			System.out.println("      |    |      ");
-		}
-		else if(!exitNorth && !exitEast && exitSouth && exitWest) {
-		//else if(ID == 8) {
+		} else if (!exitNorth && !exitEast && exitSouth && exitWest) {
+			// else if(ID == 8) {
 			System.out.println("                  ");
 			System.out.println("	     	   	  ");
 			System.out.println("___________       ");
@@ -123,9 +128,8 @@ public class Room {
 			System.out.println("      |    |	  ");
 			System.out.println("      |	   |      ");
 			System.out.println("      |    |      ");
-		}
-		else if(!exitNorth && exitEast && !exitSouth && exitWest) {
-		//else if(ID == 9) {
+		} else if (!exitNorth && exitEast && !exitSouth && exitWest) { // 1 exit.
+			// else if(ID == 9) {
 			System.out.println("                  ");
 			System.out.println("	   			  ");
 			System.out.println("__________________");
@@ -134,9 +138,8 @@ public class Room {
 			System.out.println("	  	      	  ");
 			System.out.println("	  	          ");
 			System.out.println("                  ");
-		}
-		else if(exitNorth && !exitEast && exitSouth && !exitWest) {
-		//else if(ID == 10) {
+		} else if (exitNorth && !exitEast && exitSouth && !exitWest) {
+			// else if(ID == 10) {
 			System.out.println("      |    |      ");
 			System.out.println("      |	   |	  ");
 			System.out.println("      |    |      ");
@@ -147,8 +150,8 @@ public class Room {
 			System.out.println("      |    |      ");
 		}
 		// One Way
-		else if(exitNorth && !exitEast && !exitSouth && !exitWest) {
-		//else if(ID == 11) {
+		else if (exitNorth && !exitEast && !exitSouth && !exitWest) {
+			// else if(ID == 11) {
 			System.out.println("      |    |      ");
 			System.out.println("      |	   |	  ");
 			System.out.println("      |    |      ");
@@ -157,9 +160,8 @@ public class Room {
 			System.out.println("      |    |	  ");
 			System.out.println("      |	   |      ");
 			System.out.println("      |____|      ");
-		}
-		else if(!exitNorth && !exitEast && exitSouth && !exitWest) {
-		//else if(ID == 12) {
+		} else if (!exitNorth && !exitEast && exitSouth && !exitWest) {
+			// else if(ID == 12) {
 			System.out.println("       ____       ");
 			System.out.println("      |    |      ");
 			System.out.println("      |	   |	  ");
@@ -169,9 +171,8 @@ public class Room {
 			System.out.println("      |    |	  ");
 			System.out.println("      |	   |      ");
 			System.out.println("      |    |      ");
-		}
-		else if(!exitNorth && !exitEast && !exitSouth && exitWest) {
-		//else if(ID == 13) {
+		} else if (!exitNorth && !exitEast && !exitSouth && exitWest) {
+			// else if(ID == 13) {
 			System.out.println("                  ");
 			System.out.println(" 	         	  ");
 			System.out.println("_________________ ");
@@ -180,9 +181,8 @@ public class Room {
 			System.out.println("            	  ");
 			System.out.println(" 	              ");
 			System.out.println("                  ");
-		}
-		else if(!exitNorth && exitEast && !exitSouth && !exitWest) {
-		//else if(ID == 14) {
+		} else if (!exitNorth && exitEast && !exitSouth && !exitWest) {
+			// else if(ID == 14) {
 			System.out.println("                  ");
 			System.out.println("        	   	  ");
 			System.out.println(" _________________");
@@ -191,8 +191,7 @@ public class Room {
 			System.out.println("    	          ");
 			System.out.println("                  ");
 			System.out.println("                  ");
-		}
-		else if(!exitNorth && !exitEast && !exitSouth && !exitWest) {
+		} else if (!exitNorth && !exitEast && !exitSouth && !exitWest) { // 0 exits. (the boss room)
 			System.out.println(" ________________ ");
 			System.out.println("|                |");
 			System.out.println("|                |");
@@ -203,33 +202,33 @@ public class Room {
 			System.out.println("|________________|");
 		}
 	}
-	
-	public int getRoomNumber() {
+
+	public int getRoomNumber() { // Functions that return rooms.
 		return this.roomNumber;
 	}
-	
+
 	public int getRoomNorth() {
 		return this.roomNorth;
 	}
-	
+
 	public int getRoomEast() {
 		return this.roomEast;
 	}
-	
+
 	public int getRoomSouth() {
 		return this.roomSouth;
 	}
-	
+
 	public int getRoomWest() {
 		return this.roomWest;
 	}
-	
+
 	public boolean getRoomAlreadyExplored() {
 		return this.alreadyExplored;
 	}
-	
+
 	public void setRoomAlreadyExplored(boolean alreadyExplored) {
 		this.alreadyExplored = alreadyExplored;
 	}
-	
+
 }
